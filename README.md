c0884a9f856c0e23e9cfc641510b94ae497ed201
AlexeyN <aleksgavr@gmail.com>
2022-08-10 02:40:38 +1200

    Played with urls

--- README.md
+++
@@ -1,2 +0,0 @@
-# News Portal
-List_all_com.txt - файл, в котором записан список всех команд, запускаемых в Django shell.
--- newsapp/views.py
+++ newsapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import *
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('Hello, World')
+# class AuthorList(ListView):
+#     # Указываем модель, объекты которой мы будем выводить
+#     model = Author
+#     # Поле, которое будет использоваться для сортировки объектов
+#     # ordering = 'name'
+#     # Указываем имя шаблона, в котором будут все инструкции о том,
+#     # как именно пользователю должны быть показаны наши объекты
+#     template_name = 'newsapp/authors.html'
+#     # Это имя списка, в котором будут лежать все объекты.
+#     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
+#     context_object_name = 'Authors'
+#     # queryset = Author.objects.all()
+#
+# # Create your views here.
